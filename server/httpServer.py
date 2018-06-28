03baa79ae4789bb09a08b7e971878c01daa13dfc
isakzhanov <yrysbek.isakzhanov@iaau.edu.kg>
2018-06-28 16:16:05 +0600

    final

--- server/httpServer.py
+++ server/httpServer.py
@@ -12,36 +12,47 @@ class Server:
 
     def run_server(self):
         print("Run server on host", self.host, ":", self.port)
-        self.socket.bind((self.host, self.port))
-
+        try:
+            self.socket.bind((self.host, self.port))
+        except Exception as ex:
+            print("Exception:", ex.args[0])
+            import sys
+            sys.exit(1)
         self._waiting_connection()
 
     def _generate_status_line(self, code):
         if code == 200:
-            status_line = "HTTP/1.1 200 OK\n"
+            status_line = "HTTP/1.1 200 OK\r\n"
+
+        elif code == 303:
+            status_line = "HTTP/1.1 303 See Other\r\n"
 
         elif code == 404:
-            status_line = "HTTP/1.1 404 Not Found\n"
+            status_line = "HTTP/1.1 404 Not Found\r\n"
 
         elif code == 501:
-            status_line = "HTTP/1.1 501 Not Implemented\n"
+            status_line = "HTTP/1.1 501 Not Implemented\r\n"
         else:
-            status_line = "HTTP/1.1 437 Unavailable for several reasons\n"
+            status_line = "HTTP/1.1 437 Unavailable for several reasons\r\n"
         return status_line
 
-    def _generate_header(self, code):
+    def _generate_header(self, code, location=""):
         header = self._generate_status_line(code)
         current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
-        header += "Date:" + current_date + "\n"\
-                  "Server: Simple-Python-HTTP-Server\n" \
-                  "Connection: close\n\n"
+        if code == 303:
+            header += location
+
+        header += "Date:" + current_date + "\r\n" \
+                  "Server: Simple-Python-HTTP-Server\r\n" \
+                  "Content-Type: text/html; charset=UTF-8\r\n" \
+                  "Connection: keep-alive\r\n\r\n"
+
         return header.encode()
 
     def _get_method_response(self, request_method, file_requested):
         if file_requested == "/" or file_requested == "/index.html":
             file_requested = "/index.html"
-        else:
-            file_requested = "/registration.html"
+
         file_requested = self._dir + file_requested
 
         try:
@@ -51,7 +62,6 @@ class Server:
             file_handler.close()
         except Exception as ex:
             print("Cannot find requested resource", ex.args[0])
-            response_header = self._generate_header(404)
             resource = b"<html>" \
                        b"<head>" \
                        b"<title>404</title>" \
@@ -61,38 +71,18 @@ class Server:
                        b"</body>" \
                        b"</html>"
 
+            response_header = self._generate_header(404)
+
         if request_method == "GET":
             response = response_header + resource
         else:
             response = response_header
         return response
 
-    def _html_dict(self, dictionary):
-        result = ""
-        for k, v in dictionary.items():
-            result += "<h3>{0}: {1}</h3>".format(k, v)
+    def _post_method_request(self):
+        response_header = self._generate_header(303, "Location: /successful.html\r\n")
 
-        return result.encode()
-
-    def _post_method_request(self, entity_body):
-        dictionary = {}
-        entity_body = entity_body.split("&")
-        for i in entity_body:
-            key, val = i.split("=")
-            dictionary[key] = val
-
-        resource = b"<html>" \
-                   b"<head>" \
-                   b"<title> Successful </title>" \
-                   b"</head>" \
-                   b"<body>" + self._html_dict(dictionary) + \
-                   b"</body>" \
-                   b"</html>"
-
-        response_header = self._generate_header(200)
-        response = response_header + resource
-
-        return response
+        return response_header
 
     def _waiting_connection(self):
         while True:
@@ -100,22 +90,18 @@ class Server:
             conn, adr = self.socket.accept()
             print("Connecting to adr:", adr)
 
-            data = conn.recv(2048)
-
+            data = conn.recv(4096)
             request = data.decode()
-            request = request.split()
-            request_method, request_uri, http_version = request[:3]
+            request_method, request_uri, *request = request.split()
             file_requested = request_uri.split("?")[0]
             if request_method == "GET" or request_method == "HEAD":
                 response = self._get_method_response(request_method, file_requested)
 
-                conn.send(response)
-
             elif request_method == "POST":
-                entity_body = request[-1]
-                response = self._post_method_request(entity_body)
-
-                conn.send(response)
+                response = self._post_method_request()
+            else:
+                response = self._generate_header(437)
+            conn.send(response)
 
             conn.close()
 
